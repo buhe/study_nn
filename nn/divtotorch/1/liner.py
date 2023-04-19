7db38364f71554b9ba3072a7cf649e25d413f38e
buhe <bugu1986@126.com>
2023-04-19 22:21:28 +0800

    "Update liner.py to include a linear regression model in PyTorch to predict values based on input data."

--- nn/divtotorch/1/liner.py
+++ nn/divtotorch/1/liner.py
@@ -1,8 +1,26 @@
 import torch
+xs = [1.0, 2.0, 3.0]
+ys = [2.0, 4.0, 6.0]
+w = torch.tensor([1.0], requires_grad=True)
+def forward(x):
+    # print('call forward')
+    return w * x
+def loss(x, y):
+    # print('call loss')
+    y_hat = forward(x)
+    return (y_hat - y) ** 2
 
-def forward():
-    print('call forward')
+print('predict (before training)', 4, forward(4).item())
+for epoch in range(100):
+    for x, y in zip(xs, ys):
+        l = loss(x, y)
+        l.backward()
+        print('\tgrad:', x, y, w.grad.item())
+        w.data -= 0.01 * w.grad.data # .data 不会产生计算图
+        print(w.grad.item(),'||', w.grad.data, "||")
+        w.grad.data.zero_()
+    print('progress:', epoch, l.item())
 
-def loss():
-    print('call loss')
+print('predict (after training)', 4, forward(4).item())
+
     

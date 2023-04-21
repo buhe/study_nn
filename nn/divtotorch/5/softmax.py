a7d8724b060512ec1dc6443272ad7764d92bfe9f
buhe <bugu1986@126.com>
2023-04-21 22:53:35 +0800

    "Add MNIST dataset and Softmax model implementation."

---
+++ nn/divtotorch/5/softmax.py
@@ -0,0 +1,72 @@
+import torch
+from torchvision import transforms
+from torchvision import datasets
+from torch.utils.data import DataLoader
+
+batch_size = 64
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.1307,), (0.3081,))
+])
+# 1
+train_dataset = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+test_dataset = datasets.MNIST(root='./data', train=False, download=True, transform=transform)
+test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+# 2
+class Net(torch.nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.l1 = torch.nn.Linear(784, 512)
+        self.l2 = torch.nn.Linear(512, 256)
+        self.l3 = torch.nn.Linear(256, 128)
+        self.l4 = torch.nn.Linear(128, 64)
+        self.l5 = torch.nn.Linear(64, 10)
+    
+    def forward(self, x):
+        x = x.view(-1, 784)
+        x = torch.relu(self.l1(x))
+        x = torch.relu(self.l2(x))
+        x = torch.relu(self.l3(x))
+        x = torch.relu(self.l4(x))
+        x = self.l5(x)
+        return x
+    
+model = Net()
+loss_func = torch.nn.CrossEntropyLoss()
+opt = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
+# 3
+def train(epoch):
+    running_loss = 0.0
+    for batch_idx, data in enumerate(train_loader):
+        inputs, target = data
+        opt.zero_grad()
+
+        outputs = model(inputs)
+        loss = loss_func(outputs, target)
+        loss.backward()
+        opt.step()
+
+        running_loss += loss.item()
+        if batch_idx % 300 == 299:
+            print('[%d, %5d] loss: %.3f' % (epoch + 1, batch_idx + 1, running_loss / 300))
+            running_loss = 0.0
+
+def test():
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in test_loader:
+            images, labels = data
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    print('Accuracy of the network on the 10000 test images: %d %%' % (100 * correct / total))
+
+if __name__ == '__main__':
+    for epoch in range(10):
+        train(epoch)
+        test()
+
+# print(torch.__version__)

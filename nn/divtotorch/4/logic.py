98b59a0f5de49991298ac0f153717d5b338888c4
buhe <bugu1986@126.com>
2023-04-21 18:58:42 +0800

    "Refactor model architecture to have 3 layers instead of 2 and add a Sigmoid activation function to each layer to improve prediction accuracy on diabetes data."

--- nn/divtotorch/4/logic.py
+++ nn/divtotorch/4/logic.py
@@ -5,19 +5,20 @@ xy = np.loadtxt('./diabetes.csv', delimiter=',', dtype=np.float32)
 xs = torch.from_numpy(xy[:,:-1])
 # print(xs)
 ys = torch.from_numpy(xy[:,[-1]])
-
+# print(ys)
 class LogisticRegressionModel(torch.nn.Module):
     def __init__(self):
-        super().__init__()
-        self.linear1 = torch.nn.Linear(8, 4)
-        self.linear2 = torch.nn.Linear(4, 2)
-        self.linear3 = torch.nn.Linear(2, 1)
+        super(LogisticRegressionModel, self).__init__()
+        self.linear1 = torch.nn.Linear(8, 6)
+        self.linear2 = torch.nn.Linear(6, 4)
+        self.linear3 = torch.nn.Linear(4, 1)
+        self.active = torch.nn.Sigmoid()
         
     def forward(self, x):
-        y_pred = torch.sigmoid(self.linear1(x))
-        y_pred = torch.sigmoid(self.linear2(y_pred))
-        y_pred = torch.sigmoid(self.linear3(y_pred))
-        return y_pred
+        x = self.active(self.linear1(x))
+        x = self.active(self.linear2(x))
+        x = self.active(self.linear3(x))
+        return x
     
 # trainer
 model = LogisticRegressionModel()
@@ -29,21 +30,23 @@ for i in range(100):
     y_pred = model(xs)
     loss = loss_fn(y_pred, ys)
     print(f'loss: {loss.item()}')
+    
     opt.zero_grad()
     loss.backward()
+
     opt.step()
         
-# print(f'w: {model.weight.item()}')
-# print(f'b: {model.bias.item()}')
-
-# x_test_np = np.linspace(0, 10, 100)
-# x_test = torch.tensor(x_test_np, dtype=torch.float32).view((100, 1))
-# y_test = model(x_test)
-# y_test = y_test.detach().numpy()
-
-# plt.plot(x_test_np, y_test, color='red')
-# plt.plot([0, 10], [0.5, 0.5], color='blue')
-# plt.xlabel('h')
-# plt.ylabel('p')
-# plt.grid()
-# plt.show()
+print(f'w1: {model.linear1.weight.data}')
+print(f'b1: {model.linear1.bias.data}')
+# -0.882353,-0.0653266,0.147541,-0.373737,0,-0.0938897,-0.797609,-0.933333,1
+x_test = torch.tensor([[-0.882353,-0.0653266,0.147541,-0.373737,0,-0.0938897,-0.797609,-0.933333]])
+print(f'x_test: {x_test.data}')
+y_test = model(x_test)
+# -0.882353,0.266332,-0.0163934,0,0,-0.102832,-0.768574,-0.133333,0
+# 0.0588235,0.708543,0.213115,-0.373737,0,0.311475,-0.722459,-0.266667,0
+# -0.882353,-0.145729,0.0819672,-0.414141,0,-0.207153,-0.766866,-0.666667,1
+print(f'y_pred: {y_test.item()}')
+x_test2 = torch.tensor([[-0.882353,-0.145729,0.0819672,-0.414141,0,-0.207153,-0.766866,-0.666667]])
+print(f'x_test 2: {x_test2.data}')
+y_test2 = model(x_test2)
+print(f'y_pred 2: {y_test2.item()}')

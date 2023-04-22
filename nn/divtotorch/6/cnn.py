27aa8e8d16f168606e6e2db32a16f87fbce5b581
buhe <bugu1986@126.com>
2023-04-22 20:55:25 +0800

    "Add MNIST dataset loading and CNN training and testing to project\n\nThis commit adds code to load the MNIST dataset and set up data loaders for training and testing a convolutional neural network (CNN) model. It also defines the CNN architecture, loss function, optimizer, training loop, and testing function. The code is run for 10 epochs, and the accuracy of the model on the test set is printed after each epoch."

---
+++ nn/divtotorch/6/cnn.py
@@ -0,0 +1,70 @@
+import torch
+from torchvision import transforms
+from torchvision import datasets
+from torch.utils.data import DataLoader
+
+batch_size = 64
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.1307,), (0.3081,))
+])
+# 1
+train_dataset = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+test_dataset = datasets.MNIST(root='./data', train=False, download=True, transform=transform)
+test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+# 2
+class Net(torch.nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = torch.nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = torch.nn.Conv2d(10, 20, kernel_size=5)
+        self.pooling = torch.nn.MaxPool2d(2)
+        self.fc1 = torch.nn.Linear(320, 50)
+
+    def forward(self, x):
+        batch_size = x.size(0)
+        x = self.pooling(torch.relu(self.conv1(x)))
+        x = self.pooling(torch.relu(self.conv2(x)))
+        x = x.view(batch_size, -1)
+        x = self.fc1(x)
+        return x
+    
+model = Net()
+loss_func = torch.nn.CrossEntropyLoss()
+opt = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
+# 3
+def train(epoch):
+    running_loss = 0.0
+    for batch_idx, data in enumerate(train_loader):
+        inputs, target = data
+        opt.zero_grad()
+
+        outputs = model(inputs)
+        loss = loss_func(outputs, target)
+        loss.backward()
+        opt.step()
+
+        running_loss += loss.item()
+        if batch_idx % 300 == 299:
+            print('[%d, %5d] loss: %.3f' % (epoch + 1, batch_idx + 1, running_loss / 300))
+            running_loss = 0.0
+
+def test():
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in test_loader:
+            images, labels = data
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    print('Accuracy of the network on the 10000 test images: %d %%' % (100 * correct / total))
+
+if __name__ == '__main__':
+    for epoch in range(10):
+        train(epoch)
+        test()
+
+# print(torch.__version__)
